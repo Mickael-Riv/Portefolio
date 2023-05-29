bfd3b0627775cc6b00719ce9d87d3928c5cb301e
Mickael-Riv <mickael.riviere@esiroi.re>
2023-05-29 15:16:27 +0200

    feat(PTF)init project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DescriptionComponent } from './description/description.component';
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot([
+    {path: '', component: DescriptionComponent},
+  ])],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
